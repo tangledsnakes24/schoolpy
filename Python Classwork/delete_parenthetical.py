650de4e07cc41b983752561b51a0070ae67ce49c
tangledsnakes24 <cbradshaw@commschool.org>
2024-12-02 14:26:51 -0500

    Create delete_parenthetical.py

---
+++ Python Classwork/delete_parenthetical.py
@@ -0,0 +1,36 @@
+""" Problem 1: Erase Parentheticals
+Write a function erase_parentheticals(s) that removes all parentheses and their contents from a string s.
+
+One test has been given for your convenience.
+
+You may assume that there will be no nested parentheses, and that all parentheses will be closed.
+"""
+
+
+def erase_parentheticals(s):
+    # CODE GOES HERE
+    return()
+
+print(erase_parentheticals("h(ello w)o(r)ld"))       # hold
+# MORE TESTS GO HERE
+
+
+
+
+""" Problem 2: Double Parentheticals
+Write a function double_parentheticals(s) that doubles the contents of all parentheses and in a string s.
+
+Two tests have been given for your convenience.
+
+You may assume that there will be no nested parentheses, and that all parentheses will be closed.
+"""
+
+
+def double_parentheticals(s):
+    # CODE GOES HERE
+    return()
+
+print(double_parentheticals("a(c)e(s)"))       # access
+print(double_parentheticals("sp(in)es"))       # spininess
+# MORE TESTS GO HERE
+
