839246d38a559f035eaa6526909573c6015c6a72
tangledsnakes24 <cbradshaw@commschool.org>
2024-12-04 10:18:23 -0500

    Create ASCIIART.py

---
+++ Python projects/ASCIIART.py
@@ -0,0 +1,78 @@
+""" Problem 1: Triangle """
+
+def t(n):
+    return()
+
+t(3)
+t(4)
+t(5)
+
+# .       .        .
+# ..      ..       ..
+# ...     ...      ...
+#         ....     ....
+#                  .....
+
+""" Problem 2: Pyramid """
+
+def p(n):
+    return()
+
+p(3)
+p(4)
+p(5)
+
+#                         .
+#             .          ...
+#   .        ...        .....
+#  ...      .....      .......
+# .....    .......    .........
+
+""" Problem 3: X """
+
+def x(n):
+    return()
+
+x(3)
+x(4)
+x(5)
+
+#             X...X
+#       X..X  .X.X.
+# X.X   .XX.  ..X..
+# .X.   .XX.  .X.X.
+# X.X   X..X  X...X
+
+""" Problem 4: Checkerboard """
+
+def c(n, s):
+    return()
+
+c(6, 1)
+c(3, 2)
+c(2, 3)
+c(1, 6)
+
+# X.X.X.    XX..XX    XXX...    XXXXXX
+# .X.X.X    XX..XX    XXX...    XXXXXX
+# X.X.X.    ..XX..    XXX...    XXXXXX
+# .X.X.X    ..XX..    ...XXX    XXXXXX
+# X.X.X.    XX..XX    ...XXX    XXXXXX
+# .X.X.X    XX..XX    ...XXX    XXXXXX 
+
+
+
+""" Problem 5: Squares """
+
+def s(n):
+    return()
+
+s(3)
+s(4)
+s(5)
+
+#                .....
+#        ....    .XXX.
+# ...    .XX.    .X.X.
+# .X.    .XX.    .XXX.
+# ...    ....    .....
