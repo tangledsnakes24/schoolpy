eabd0cf550044963938e3574887202318a271b2f
tangledsnakes24 <cbradshaw@commschool.org>
2025-01-15 10:27:47 -0500

    Update sortday2.py

--- Python Classwork/sortday2.py
+++ Python Classwork/sortday2.py
@@ -1,8 +1,12 @@
 """ Sorting Day II: Insertion and Bubble """
 
 # Problem 0: Insert
-
+#while list isnt empty:
+#remove 0th element of l
+#insert it into new_l in correct position
+#repeat until sorted
 def insert(element, l):
+    
     return()
 
 print("Testing Insert...")
@@ -11,6 +15,7 @@ print(insert(2, [1, 3, 4]))            # [1, 2, 3, 4]
 print(insert(3, [1, 2, 4]))            # [1, 2, 3, 4]
 print(insert(4, [1, 2, 3]))            # [1, 2, 3, 4]
 print()
+print("____________________________")
 
 # Problem 1: Insertion Sort
 
@@ -27,6 +32,7 @@ print(insertion_sort([1, 2, 3, 4, 5]))  # [1, 2, 3, 4, 5]
 print(insertion_sort([5, 4, 3, 2, 1]))  # [1, 2, 3, 4, 5]
 print(insertion_sort([2, 4, 1, 3, 5]))  # [1, 2, 3, 4, 5]
 print()
+print("____________________________")
 
 # Problem 2: Swap
 
@@ -38,6 +44,7 @@ print(swap([2, 1, 3, 4], 0, 1))         # [1, 2, 3, 4]
 print(swap([1, 3, 2, 4], 1, 2))         # [1, 2, 3, 4]
 print(swap([1, 2, 4, 3], 2, 3))         # [1, 2, 3, 4]
 print()
+print("____________________________")
 
 # Problem 3: Bubble Sort
 
@@ -54,3 +61,4 @@ print(bubble_sort([1, 2, 3, 4, 5]))  # [1, 2, 3, 4, 5]
 print(bubble_sort([5, 4, 3, 2, 1]))  # [1, 2, 3, 4, 5]
 print(bubble_sort([2, 4, 1, 3, 5]))  # [1, 2, 3, 4, 5]
 print()
+print("____________________________")
