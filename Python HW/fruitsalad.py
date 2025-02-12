5fbdfc096accfe91cc878a9aac3f4c24cee45ae6
tangledsnakes24 <cbradshaw@commschool.org>
2025-02-12 10:23:35 -0500

    Create fruitsalad.py

---
+++ Python HW/fruitsalad.py
@@ -0,0 +1,56 @@
+""" Day 1 Homework: Fruit Salads """
+
+salad1 = {"apple": 20,
+          "banana": 30}
+
+salad2 = {"cherry": 100}
+
+salad3 = {"apple": 1,
+          "banana": 1,
+          "cherry": 1,
+          "dates": 1}
+
+salad4 = {"cherry": 30,
+          "elderberry": 20}
+
+# Problem 1: Total Cost
+
+def total_cost(prices, recipe):
+    return()
+
+prices = {"apple": 3,
+          "banana": 2,
+          "cherry": 4,
+          "dates": 5,
+          "elderberry": 6}
+
+print(total_cost(prices, salad1))       # 120 (20 * 3 + 30 * 2)
+print(total_cost(prices, salad2))       # 400 (100 * 4)
+print(total_cost(prices, salad3))       # 14 (3 + 2 + 4 + 5)
+print(total_cost(prices, salad4))       # 240 (30 * 4 + 20 * 6)
+
+# Problem 2: Makeable
+
+pantry = {"apple": 40,
+          "banana": 50,
+          "cherry": 20,
+          "dates": 70,
+          "elderberry": 0}
+
+def makeable(pantry, recipe):
+    return()
+
+print(makeable(pantry, salad1))       # True
+print(makeable(pantry, salad2))       # False (not enough cherries)
+print(makeable(pantry, salad3))       # True
+print(makeable(pantry, salad4))       # False (not enough cherries or elderberries)
+
+# Problem 3: Batches
+
+def batches(pantry, recipe):
+    return()
+
+print(batches(pantry, salad1))       # 1 (enough apples for 2, but not enough bananas)
+print(batches(pantry, salad2))       # 0 (not enough cherries)
+print(batches(pantry, salad3))       # 20 (limited by cherries)
+print(batches(pantry, salad4))       # 0 (not enough cherries or elderberries)
