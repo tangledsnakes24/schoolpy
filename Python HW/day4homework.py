cbc175ae1fffb37557aa46e848fada0788dc0175
tangledsnakes24 <cbradshaw@commschool.org>
2025-03-02 19:54:37 -0500

    Update day4homework.py

--- Python HW/day4homework.py
+++ Python HW/day4homework.py
@@ -3,11 +3,15 @@ Write a function that takes in a string and returns a dictionary that counts
 the number of times each letter appears in the string, ignoring spaces.
 """
 
-def letter_frequency(s):
-
-    #for letter in s:
-    #make a counter, loop over string, for each instance of the letter, counter += 1
-    return()
+def freq(s):
+    new_dict = {}
+    for letter in s:
+        if letter != " ":
+            if letter not in new_dict:
+                new_dict[letter] = 1
+            else:
+                new_dict[letter] += 1    
+    return(new_dict)
 
 print(freq("HELLO"))            # {"H": 1, "E": 1, "L": 2, "O": 1}
 print(freq("WORLD"))            # {"W": 1, "O": 1, "R": 1, "L": 1, "D": 1}
@@ -25,7 +29,26 @@ For example, OS is not a bigram in "NO SPACES."
 """
 
 def bigram_frequency(s):
-    return()
+    bigram_dict = {}
+    
+    in_word = False
+    prev_char = ""
+    
+    for char in s:
+        if char != " ":  # Only process letters, skipping spaces
+            if in_word:
+                bigram = prev_char + char
+                if bigram not in bigram_dict:
+                    bigram_dict[bigram] = 1
+                else:
+                    bigram_dict[bigram] += 1
+            prev_char = char
+            in_word = True
+        else:
+            in_word = False  # Reset when encountering a space
+    
+    return bigram_dict
+    return(bigram_dict)
 
 print(bigram_frequency("HELLO"))            # {"HE": 1, "EL": 1, "LL": 1, "LO": 1}
 print(bigram_frequency("BANANA"))           # {"BA": 1, "AN": 2, "NA": 2}
