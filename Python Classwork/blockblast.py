cb5b6d9343f44e87fe7718e0618ad13863db64d8
tangledsnakes24 <cbradshaw@commschool.org>
2025-03-26 10:50:39 -0400

    Update blockblast.py

--- Python Classwork/blockblast.py
+++ Python Classwork/blockblast.py
@@ -1,60 +1,46 @@
 """ Block Blast """
-
 square = [[0, 0],
           [0, 1],
           [1, 0],
           [1, 1]]
-
 horiz_line = [[0, 0],
               [0, 1],
               [0, 2],
               [0, 3]]
-
 vert_line = [[0, 0],
              [1, 0],
              [2, 0],
              [3, 0]]
-
 elbow1 = [[0, 0],
           [1, 0],
           [1, 1]]
-
 elbow2 = [[0, 0],
           [0, 1],
           [1, 1]]
-
 elbow3 = [[0, 0],
           [0, 1],
           [1, 0]]
-
 elbow4 = [[0, 1],
           [1, 0],
           [1, 1]]
-
 upwardt = [[1, 0],
            [0, 1],
            [1, 1],
            [1, 2]]
-
 """
 Elbows:
-
 e1     e2     e3     e4
 x.     xx     xx     .x           
 xx     .x     x.     xx
-
 """
-
 empty_grid = [[".", ".", ".", "."],
               [".", ".", ".", "."],
               [".", ".", ".", "."],
               [".", ".", ".", "."]]
-
 diag_grid = [["x", ".", ".", "."],
              [".", "x", ".", "."],
              [".", ".", "x", "."],
              [".", ".", ".", "x"]]
-
 def can_place(piece, grid, x_ref, y_ref):
     for coord in piece:
         x_disp = coord[0]
@@ -85,10 +71,7 @@ print(can_place(upwardt, diag_grid, 0, 2))  #true
 print(can_place(upwardt, diag_grid, 2, 0))  #false
 print(can_place(upwardt, diag_grid, 1, 1))  #false
 print(can_place(upwardt, diag_grid, 2, 2))  #false
-
 def place(piece, grid, x, y):
     return()
-
 def clear(grid):
     return()
-
