8e1e9fcb6e0d9e4cb1ee93270e3b8691703c0c50
tangledsnakes24 <cbradshaw@commschool.org>
2025-04-02 10:43:03 -0400

    Update randomnessday3.py

--- Python Classwork/randomnessday3.py
+++ Python Classwork/randomnessday3.py
@@ -1,5 +1,5 @@
 """ Input/Output """
-
+import random
 name = input("What is your name? ")
 print("Hello there, " + name + "!")
 
@@ -16,8 +16,8 @@ print("the sum of you two numbers is......." + str(numbersum))
 # Problem 2: Guess My Number
 
 print("I'm thinking of a number between 1 and 20.")
-print("Guess a number, and I'll tell you if you're too high, too low, or correct")
-secretnum = 11
+print("Guess a number between 0 and 30, and I'll tell you if you're too high, too low, or correct")
+secretnum = random.randint(1, 30)
 guess = input("guess my number: ")
 while True:
     guess = input("guess my number: ")
