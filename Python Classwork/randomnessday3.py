0bd3d03230416626c0b2ec30b3dddfbf8a440aa5
tangledsnakes24 <cbradshaw@commschool.org>
2025-04-02 10:21:07 -0400

    Create randomnessday3.py

---
+++ Python Classwork/randomnessday3.py
@@ -0,0 +1,21 @@
+""" Input/Output """
+
+name = input("What is your name? ")
+print("Hello there, " + name + "!")
+
+
+# Problem 1: Calculator
+
+print("If you give me two numbers to add, I'll tell you their sum.")
+
+
+# Problem 2: Guess My Number
+
+print("I'm thinking of a number between 1 and 20.")
+print("Guess a number, and I'll tell you if you're too high, too low, or correct")
+
+
+# Problem 3: Green Glass Door
+
+print("Many items that will open the Green Glass Door, but not all!")
+print("If you can name 3 objects in a row that will, I'll let you through.")
