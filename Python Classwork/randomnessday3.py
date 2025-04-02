03f29d242c4c7f458f3fba8f90db13713af32ea5
tangledsnakes24 <cbradshaw@commschool.org>
2025-04-02 10:52:03 -0400

    Update randomnessday3.py

--- Python Classwork/randomnessday3.py
+++ Python Classwork/randomnessday3.py
@@ -17,17 +17,26 @@ print("the sum of you two numbers is......." + str(numbersum))
 
 print("Guess a number between 0 and 30, and I'll tell you if you're too high, too low, or correct")
 secretnum = random.randint(1, 30)
-guess = input("guess my number: ")
 while True:
     guess = input("guess my number: ")
-    if int(guess) == secretnum:
-        print("you got it!")
-        break
-    if int(guess) > secretnum:
-        print("too big")
-    if int(guess) < secretnum:
-        print("too small")
 
+    while True:
+        guess = input("guess my number: ")
+        if int(guess) <= 30 and int(guess) >= 1:
+            if int(guess) == secretnum:
+                print("you got it!")
+                break
+            if int(guess) > secretnum:
+                print("too big")
+            if int(guess) < secretnum:
+                print("too small")
+        else:
+            print("are you stupid! can you read! thats not between 1 and 30! try again!")
+            quit()
+    keep_going = input("do you want to keep going? 1 for yes, 0 for no")
+    if keep_going == "0":
+        print("see you later")
+        break
 # Problem 3: Green Glass Door
 
 print("Many items that will open the Green Glass Door, but not all!")
